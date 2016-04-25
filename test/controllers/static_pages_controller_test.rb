d589df0e37a24c094025bd5d11a9f41e32c0e108
sunbather <mjplattner@gmail.com>
2016-04-25 18:24:37 -0400

    Finished Static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Eat brains"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Eat brains"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Eat brains"
   end
 
 end
