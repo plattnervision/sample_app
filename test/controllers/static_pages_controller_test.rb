34f9714a195e7b0b44062b59c43f54e82c4641ee
sunbather <mjplattner@gmail.com>
2016-05-03 22:36:44 -0400

    Added some CSS styling via bootstraps
    - added boottrap and sass gems
    - added links and routes for all of the current paths except login
    - added a sick zombie pic

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,38 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+   @base_title = "Eat brains"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Eat brains"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Eat brains"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Eat brains"
+    assert_select "title", "About | #{@base_title}"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+  # add contact page
+  ## write test to get it and test for a success message
+  ### create route for contact
+  #### create static html page for contact
+      # add title and provide statements
 
 end
