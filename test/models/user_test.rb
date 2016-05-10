46fd10dd52636efc66dcdc1a6fc019372eaa7140
sunbather <mjplattner@gmail.com>
2016-05-09 21:35:03 -0400

    Few migrations and authenticateion
    - Migrations to add has_secure_password
    - Migration to index email column to make searching faster
    - added tests to validate emails and passwords
    - BABABOOEY

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                                        password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -45,6 +46,31 @@ class UserTest < ActiveSupport::TestCase
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} shoudl be invalid"
-    end 
+    end
   end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "passowrd should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
 end
