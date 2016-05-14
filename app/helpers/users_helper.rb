7a69978d1eba2915059cf0d96aa0a5b16ddb423e
sunbather <mjplattner@gmail.com>
2016-05-14 19:48:22 -0400

    sign up form and tests
    created a working sign up form using form_for
    created a few tests to make sure invalid submissions are not accepted
    add some bootstraps css to make the form not as ugly

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+  # what is this doing?
+  # assigns gravatar_id to the MD5 Hash of the lower case version
+  # of the user.email.downcase(user.email is passed in from the view)
+  # The Digest gem is used to hash shit
+  # Digest::MD5::hexdigest(what it is you want to hash)
+  # instantiaties this with the gravatar url
+  # creates an image tag which is used as alt text 
 end
