f933987d5a8a0b7cab053176a9ea97da342c42c1
sunbather <mjplattner@gmail.com>
2016-05-16 21:20:23 -0400

    Finish sign up form
    add test for succesful sign up
    add success and failure message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      flash[:success] = "LET ME EAT UR BRAINZ, COOL"
+      redirect_to @user
     else
       render 'new'
     end
