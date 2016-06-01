72d307d67d98bd33dec1695f1ee374c9da41151c
sunbather <mjplattner@gmail.com>
2016-05-31 21:53:54 -0400

    user controller index
    add user/index fiew so folks can see other folks
    add gems to allow pagination and css for that pagination
    add index method to controller so it can be called
    add a few tests for user/index so it always workz

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,10 @@
 module UsersHelper
-  def gravatar_for(user)
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
-  # what is this doing?
-  # assigns gravatar_id to the MD5 Hash of the lower case version
-  # of the user.email.downcase(user.email is passed in from the view)
-  # The Digest gem is used to hash shit
-  # Digest::MD5::hexdigest(what it is you want to hash)
-  # instantiaties this with the gravatar url
-  # creates an image tag which is used as alt text 
 end
